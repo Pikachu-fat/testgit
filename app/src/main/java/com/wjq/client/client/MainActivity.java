5fa7e619cf69dcb9d87b6d9bc1a9096314d11aa3
jqwei3 <jawei3@iflytek.com>
2019-11-24 21:01:15 +0800

    v1.1

--- app/src/main/java/com/wjq/client/client/MainActivity.java
+++ app/src/main/java/com/wjq/client/client/MainActivity.java
@@ -41,6 +41,7 @@ public class MainActivity extends AppCompatActivity {
         bindKDXF.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                // v1.0
                 for (int i =0;i<3;i++){
                     Intent intent = new Intent("com.cmcc.tvclient.kdxfbindService");
                     startService(intent);
